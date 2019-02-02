898fcad5315dae1fc724504707f57f2c3bbe0a8b
gino <gino@MM1NB07277701.Supporting.corp.bankmandiri.co.id>
2019-02-02 21:22:31 +0700

    Initiate

--- src/galaxy/merchant/trading/guide/GalaxyMerchantTradingGuide.java
+++ src/galaxy/merchant/trading/guide/GalaxyMerchantTradingGuide.java
@@ -15,6 +15,10 @@ public class GalaxyMerchantTradingGuide {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        
+        
         // TODO code application logic here
     }
     
